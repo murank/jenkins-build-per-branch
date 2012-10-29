d24bb5ba7f692f3837a06a8f960221751c86e4f8
titas.norkunas <titas.norkunas@gmail.com>
2012-10-29 15:59:20 +0200

    move query string to separate param (gets encoded otherwise)

--- src/main/groovy/com/assembla/jenkins/Api.groovy
+++ src/main/groovy/com/assembla/jenkins/Api.groovy
@@ -43,7 +43,7 @@ class Api {
         // description:Nice try, target_space_tool_id:dicbBK8rqr4ycHeJe7dGu1, updated_at:2012-08-29T05:21:19-07:00, id:155,
         // created_at:2012-08-29T05:21:19-07:00]]
 
-        def response = get(path: "v1/spaces/$spaceId/space_tools/$spaceToolId/merge_requests.json?status=open")
+        def response = get(path: "v1/spaces/$spaceId/space_tools/$spaceToolId/merge_requests.json", query: [status: "open"])
         def result = []
 
         if (response.status != 204) {
